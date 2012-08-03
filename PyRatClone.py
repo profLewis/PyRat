27caa170d408ad3e2848d6e88169d487048a7a1a
profLewis <dr.p.e.lewis@gmail.com>
2012-08-03 15:04:00 +0100

    Fixed matrices in clone

--- PyRatClone.py
+++ PyRatClone.py
@@ -69,25 +69,44 @@ class PyRatClone(PyRatBox):
     and do hierarchical intersections
     '''
     # transform ray
-    try:
-      #import pdb;pdb.set_trace()
-      transformed_ray = ray.copy()
-      transformed_ray.origin = np.array(np.matrix(ray.origin) * self.matrix).flatten()
-      transformed_ray.direction = np.array(np.matrix(ray.direction) * self.matrix).flatten()
-      mod_ray_direction=np.dot(transformed_ray.direction,transformed_ray.direction)
-      transformed_ray.direction /= mod_ray_direction
-    except:
-      transformed_ray = ray
-    try:
-      transformed_ray.origin += self.offset
-    except:
-      pass
+    transformed_ray = ray.copy()
+
     try:
       transformed_ray.sourceOfRay = ray.sourceOfRay
     except:
       pass
-    return PyRatBox.intersects(self,\
-                               transformed_ray,closest=closest)
+
+    try:
+      transformed_ray.origin -= self.offset
+    except:
+      pass
+
+    #import pdb;pdb.set_trace()
+    try:
+      transformed_ray.direction = np.array(np.matrix(ray.direction) * self.matrix.T).flatten()
+      mod_ray_direction=np.sqrt(np.dot(transformed_ray.direction,transformed_ray.direction))
+      transformed_ray.direction /= mod_ray_direction
+
+      #import pdb;pdb.set_trace()
+      transformed_ray.origin = np.array(np.matrix(transformed_ray.origin) * self.matrix.T).flatten()
+      # to account for scaling effects
+      transformed_ray.origin /= (mod_ray_direction*mod_ray_direction)
+      transformed_ray.length /= mod_ray_direction
+    except:
+      pass
+
+    hit,thisRay  = PyRatBox.intersects(self,transformed_ray,closest=closest)
+    if hit:
+      try:
+        thisRay.object.localNormal = np.array(np.matrix(thisRay.object.localNormal) * self.matrix).flatten()
+        mod = np.sqrt(np.dot(thisRay.object.localNormal,thisRay.object.localNormal))
+        thisRay.object.localNormal /= mod
+        thisRay.length *= mod_ray_direction
+        thisRay.origin = ray.origin
+        thisRay.direction = ray.direction
+      except:
+        pass
+    return hit,thisRay
 
 def main():
   '''
@@ -103,6 +122,7 @@ def main():
   from PyRatBox import PyRatBox
   from PyRatSpheroid import PyRatSpheroid
   from PyRatClone import PyRatClone
+  from PyRatEllipsoid import PyRatEllipsoid
 
   min = [-0.5,-0.5,2]
   extent = [1,2,1]
@@ -114,11 +134,22 @@ def main():
   info = {'verbose':True}
   sph = PyRatSpheroid(centre,radius,info=info)
 
+  base = np.array(min) + np.array(extent) *0.5
+  radius = [0.25,0.5,0.5]
+  info = {'verbose':True,'lad':10.}
+  ell = PyRatEllipsoid(base,radius,info=info)
+
   clone = PyRatClone(np.zeros(3),None)
   clone.thisGroup = None
-  clone.offset = np.array([1.0,0,0])
+  clone.offset = np.array([0.0,0.0,-0.5])
   clone.matrix = np.eye(3)
-  clone.contents = [box,sph]
+  c = np.cos(45*np.pi/180.)
+  s = np.sin(45*np.pi/180.)
+  clone.matrix[1,1] = clone.matrix[0,0] = c
+  clone.matrix[0,1] = -s
+  clone.matrix[1,0] = s
+
+  clone.contents = [box,sph,ell]
 
   name = str(globals()['__file__'].split('.')[0])  
   test(min,extent,obj=clone,info=info,type=name)
